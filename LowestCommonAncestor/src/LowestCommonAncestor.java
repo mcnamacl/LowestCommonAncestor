2bda79e95add2a138fb6014e1c1f2025b008dea4
mcnamacl <mcnamacl@tcd.ie>
2019-10-11 23:32:25 +0100

    Filled in the functions for LowestCommonAncestor

--- LowestCommonAncestor/src/LowestCommonAncestor.java
+++ LowestCommonAncestor/src/LowestCommonAncestor.java
@@ -10,12 +10,31 @@ public class LowestCommonAncestor {
     // x and y where both x and y are present in the Binary Search Tree
     public Node LCARecursive(Node root, int x, int y)
     {
-        return null;
+        // if both x and y is smaller than root, LCA exists in left subtree
+        if (root.data > Integer.max(x, y)) {
+            return LCARecursive(root.left, x, y);
+        }
+
+        // if both x and y is greater than root, LCA exists in right subtree
+        else  if (root.data < Integer.min(x, y)) {
+            return LCARecursive(root.right, x, y);
+        }
+
+        // if one key is greater (or equal) than root and one key is smaller
+        // (or equal) than root, then the current node is LCA
+        return root;
     }
 
     // Print Lowest Common Ancestor of two nodes in a BST
     public Node LCA(Node root, int x, int y)
     {
-        return null;
+        // return if tree is empty or either x or y is not present
+        // in the tree
+        if (root == null || !binaryTree.search(root, x) || !binaryTree.search(root, y)) {
+            return null;
+        }
+
+        // lca stores lowest common ancestor of x and y
+        return LCARecursive(root, x, y);
     }
 }
