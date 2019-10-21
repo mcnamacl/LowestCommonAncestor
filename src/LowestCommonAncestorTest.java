92f4a046829c4d24e564f9af9ff0bb758adfa46e
mcnamacl <mcnamacl@tcd.ie>
2019-10-21 15:32:17 +0100

    Added in a new test case and made some modifications.

--- src/LowestCommonAncestorTest.java
+++ src/LowestCommonAncestorTest.java
@@ -18,8 +18,8 @@ public class LowestCommonAncestorTest {
         for(int pcrawl : dag.dag.adjListArray[0]) {
             nodes.add(pcrawl);
         }
-        assertEquals("There is an edge between 0 and 1", 1, (int)nodes.get(0));
-        assertEquals("There is an edge between 0 and 2", 2, (int)nodes.get(1));
+        assertEquals("There is an edge between 0 and 1.", 1, (int)nodes.get(0));
+        assertEquals("There is an edge between 0 and 2.", 2, (int)nodes.get(1));
     }
 
     @Test
@@ -36,9 +36,51 @@ public class LowestCommonAncestorTest {
         //    /  \   /
         //   4     3
         int lcaNode = dag.LCA(dag.dag, 1, 2);
-        assertEquals("The lowest common ancestor should be 0", 0, lcaNode);
+        assertEquals("The lowest common ancestor should be 0.", 0, lcaNode);
         lcaNode = dag.LCA(dag.dag, 3, 4);
-        assertEquals("The lowest common ancestor should be 1", 1, lcaNode);
+        assertEquals("The lowest common ancestor should be 1.", 1, lcaNode);
+        lcaNode = dag.LCA(dag.dag, 1, 3);
+        assertEquals("The lowest common ancestor should be 1.", 1, lcaNode);
+    }
+
+    @Test
+    public void testLCALarge(){
+        LowestCommonAncestor dag = new LowestCommonAncestor(10);
+        dag.addEdge(0,1);
+        dag.addEdge(0,2);
+        dag.addEdge(1,3);
+        dag.addEdge(2,3);
+        dag.addEdge(1,4);
+        dag.addEdge(4,5);
+        dag.addEdge(4,6);
+        dag.addEdge(3,7);
+        dag.addEdge(2,8);
+        dag.addEdge(3,9);
+        //          0
+        //        /  \
+        //      1     2
+        //    /  \   / \
+        //   4     3    8
+        //  / \   / \
+        // 5   6 9   7
+        int lcaNode = dag.LCA(dag.dag, 1, 2);
+        assertEquals("The lowest common ancestor should be 0.", 0, lcaNode);
+        lcaNode = dag.LCA(dag.dag, 3, 4);
+        assertEquals("The lowest common ancestor should be 1.", 1, lcaNode);
+        lcaNode = dag.LCA(dag.dag, 1, 3);
+        assertEquals("The lowest common ancestor should be 1.", 1, lcaNode);
+        lcaNode = dag.LCA(dag.dag, 9, 2);
+        assertEquals("The lowest common ancestor should be 2.", 2, lcaNode);
+        lcaNode = dag.LCA(dag.dag, 5, 6);
+        assertEquals("The lowest common ancestor should be 4.", 4, lcaNode);
+        lcaNode = dag.LCA(dag.dag, 3, 8);
+        assertEquals("The lowest common ancestor should be 2.", 2, lcaNode);
+        lcaNode = dag.LCA(dag.dag, 5, 7);
+        assertEquals("The lowest common ancestor should be 1.", 1, lcaNode);
+        lcaNode = dag.LCA(dag.dag, 8, 5);
+        assertEquals("The lowest common ancestor should be 0.", 0, lcaNode);
+        lcaNode = dag.LCA(dag.dag, 6, 9);
+        assertEquals("The lowest common ancestor should be 1.", 1, lcaNode);
     }
 
     @Test
@@ -54,6 +96,9 @@ public class LowestCommonAncestorTest {
         dag.addEdge(0,1);
         dag.addEdge(1,2);
         dag.addEdge(2, 0);
+        //      0
+        //    /   \
+        //   1  -  2
         int lcaNode = dag.LCA(dag.dag, 0, 1);
         assertEquals("The function should return -1 as the graph is cyclic and therefore invalid.", -1, lcaNode);
     }
