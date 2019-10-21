eb74d8817d7d5bcb3d8f7c01a87dad3e1a7baf19
mcnamacl <mcnamacl@tcd.ie>
2019-10-21 13:23:51 +0100

    Added in two new test cases.

--- src/LowestCommonAncestorTest.java
+++ src/LowestCommonAncestorTest.java
@@ -64,4 +64,25 @@ public class LowestCommonAncestorTest {
         int lcaNode = dag.LCA(dag.dag, 0, 0);
         assertEquals("The function should return 0 as the LCA of the same node is itself.", 0, lcaNode);
     }
+
+    @Test
+    public void testLCAOneNode(){
+        LowestCommonAncestor dag = new LowestCommonAncestor(1);
+        int lcaNode = dag.LCA(dag.dag, 0, 1);
+        assertEquals("The function should return -1 as there is only one node.", -1, lcaNode);
+    }
+
+    @Test
+    public void testLCAThreeNodes(){
+        LowestCommonAncestor dag = new LowestCommonAncestor(3);
+        dag.addEdge(0,1);
+        dag.addEdge(0,2);
+        //      0
+        //    /  \
+        //  1     2
+        int lcaNode = dag.LCA(dag.dag, 0, 1);
+        assertEquals("The function should return 0 as that is the LCA of 0 and 1.", 0, lcaNode);
+        lcaNode = dag.LCA(dag.dag, 1, 2);
+        assertEquals("The function should return 0 as that is the LCA of 1 and 2.", 0, lcaNode);
+    }
 }
