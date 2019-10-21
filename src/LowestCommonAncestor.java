12c7ac94133fda6848f532aa0a0b7bd6909e8482
mcnamacl <mcnamacl@tcd.ie>
2019-10-21 16:59:00 +0100

    Added in skeleton code for LCA.
    Added in potential needed data structures also.

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -1,15 +1,24 @@
 public class LowestCommonAncestor {
 
     DAG dag;
+    int parent[];
+    boolean visited[];
+    int noVertices;
 
     LowestCommonAncestor(int noVertices) {
         this.dag = new DAG(noVertices);
+        this.noVertices = noVertices;
+        this.parent = new int[noVertices];
+        this.visited = new boolean[noVertices];
     }
 
     // Adds an edge to an directed graph
     void addEdge(int src, int dest) {
         // Add an edge from src to dest.
         dag.adjListArray[src].add(dest);
+    }
+
+    void GetParents(int node , int par){
     }
 
     int LCA(DAG dag, int nodeOne, int nodeTwo){
