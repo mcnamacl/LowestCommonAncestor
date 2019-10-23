0dc8fc881871a67604c4f3012f30e11d81252f7f
mcnamacl <mcnamacl@tcd.ie>
2019-10-23 22:19:43 +0100

    Added in extra functions to LCA.

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -10,10 +10,18 @@ public class LowestCommonAncestor {
     public LowestCommonAncestor() {
         this.dag = new DirectedAcyclicGraph<>(DefaultEdge.class);
     }
-    
+
     int LCA(int v, int w) {
         Set<Integer> firstAncestors = dag.getAncestors(v);
         Set<Integer> secondAncestors = dag.getAncestors(w);
+        return -1;
+    }
+
+    public int digraphVertexDepth(int root, int vertex) {
+        return -1;
+    }
+
+    int digraphVertexDepthRecursive(int currentVertex, int targetVertex, int depth){
         return -1;
     }
 }
